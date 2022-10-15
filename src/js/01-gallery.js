72ed9333c245533f6460648ff778e30f045f6690
Viktor <jude.peper@gmail.com>
2022-10-15 20:00:32 +0200

    style import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 console.log(SimpleLightbox);
 console.log(galleryItems);
